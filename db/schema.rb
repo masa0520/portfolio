f32aabfaef520bd5bf1b8e1619fa43c908bc612f
masa0520 <masahiro130520@icloud.com>
2022-10-23 16:36:43 +0900

    [add] Business_hourモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_23_025901) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_23_073506) do
+  create_table "business_hours", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "day"
+    t.integer "open"
+    t.integer "close"
+    t.integer "off"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
