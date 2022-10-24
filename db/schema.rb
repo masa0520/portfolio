4039b775985f98457c0ce84e0d1d8356a762e476
masa0520 <masahiro130520@icloud.com>
2022-10-24 22:37:53 +0900

    [add] Likeモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_23_073506) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_132136) do
   create_table "business_hours", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "day"
     t.integer "open"
     t.integer "close"
     t.integer "off"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
