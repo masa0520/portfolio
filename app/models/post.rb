3b081533910c5bad3205d85d57426518cdb6bb53
masa0520 <masahiro130520@icloud.com>
2022-11-10 21:30:55 +0900

    [add] Postを作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  belongs_to :user
-  belongs_to :restaurant
+  #belongs_to :user
+  #belongs_to :restaurant
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 end
