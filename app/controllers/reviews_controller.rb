578c05ecc07b7b812c37f2173cad95ab5859056a
masa0520 <masahiro130520@icloud.com>
2022-12-29 19:40:58 +0900

    [add] reviewコントローラーを編集

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,15 @@
 class ReviewsController < ApplicationController
+  def create
+    @review = current_user.reviews.build(comment_params)
+    @review.save
+  end
+
+  def destroy
+    @review = current_user.reviews.find(params[:id])
+    @review.destroy!
+  end
+
+  def private
+    params.require(:review).permit(:comment).merge(post_id: params[:post_id])
+  end
 end
